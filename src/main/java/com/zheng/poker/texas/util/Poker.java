51fc16da55330c90380443817e7eeb55ec5aeaf4
zhengjiandi <1552409217@qq.com>
2016-10-12 22:38:57 +0800

    - 添加新功能

--- src/main/java/com/zheng/poker/texas/util/Poker.java
+++ src/main/java/com/zheng/poker/texas/util/Poker.java
@@ -1,15 +1,25 @@
 package com.zheng.poker.texas.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by DJ on 2016/10/11.
  */
 public class Poker {
     private Color color;
-    private int point;
+    private int point = 2;
+    private boolean flag=true;
+    private volatile static List<Poker> pokers;
 
-    public Poker(Color color,int point){
+
+    public Poker(Color color, int point) {
         this.setColor(color);
-        this.setPoint(point);
+        try {
+            this.setPoint(point);
+        } catch (PokerException e) {
+            e.printStackTrace();
+        }
     }
 
     public Color getColor() {
@@ -24,7 +34,44 @@ public class Poker {
         return point;
     }
 
-    public void setPoint(int point) {
+    public void setPoint(int point) throws PokerException {
+        if (point < 2 || point > 14)
+            throw new PokerException("The point out of bound!");
         this.point = point;
+    }
+
+    public static List<Poker> getAllPoker() {
+        List<Poker> allPoker = new ArrayList<Poker>();
+        for (int i = 2; i < 15; i++) {
+            for (Color color : Color.values()) {
+                allPoker.add(new Poker(color, i));
+            }
+        }
+        return allPoker;
+    }
+
+
+    public static List<Poker> getPokers() {
+        if (pokers == null) {
+            synchronized (Poker.class) {
+                if (pokers == null) {
+                    pokers = getAllPoker();
+                }
+            }
+        }
+        return pokers;
+    }
+
+    @Override
+    public String toString() {
+        return this.color.toString() + " " + point;
+    }
+
+    public int getIndex() {
+        return (this.point - 2) * 4 + this.color.ordinal();
+    }
+
+    public Poker getPokerByIndex(int index){
+        return getPokers().get(index);
     }
 }
